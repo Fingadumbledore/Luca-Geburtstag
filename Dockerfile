f52585a64b63b8be2bb6fb21a0657ba62c65f8cd
datewat <thomaszimmer524@gmail.com>
2022-03-12 09:45:49 +0100

    changed directory logic in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM ubuntu:latest
 
 RUN apt update -y && apt upgrade -y
-RUN apt install -y python3 python3-pip
+RUN apt install -y python3
 
 ADD sites/ /sites
-RUN cd /sites
 
 EXPOSE 8000
 
-CMD [ "python3", "-m", "http.server", "8000" ]
+CMD [ "python3", "-m", "http.server", "--directory", "sites", "8000" ]
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install docker-compse python3  # installiert docker-compose und python3, um sicher zu gehen
 
-docker build -t luca .                  # baut docker container mit namen luca
+docker build -t luca -f Dockerfile .                  # baut docker container mit namen luca
 docker run -it -p 8000:8000 luca        # startet docker container
