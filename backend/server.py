dd88720c19fe244bf86762a0dbe422ba917c4e74
fingadumbledore <fingadumbledore@gmail.com>
2022-03-12 12:02:35 +0100

    modified:   backend/server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,37 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import http.server
 import socketserver
+import time
 
 PORT = 8000
+
+class Serve(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'frontend/sites/index.html'
+        try:
+                file_to_open = open(self.path[1:]).read()
+                self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(400)
+            print(file_to_open , "not found")
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+    def do_POST(self):
+
+        try:
+            self.send_responses(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            date = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))
+            self.wfile(bytes('{"time": "' + date + '"}',"utf-8"))
+        except:
+                self.send_response(400)
+                print("POST error")
+
+
+httpd = HTTPServer(('localhost', 8000), Serve)
+print ("server is now running")
+httpd.serve_forever()
