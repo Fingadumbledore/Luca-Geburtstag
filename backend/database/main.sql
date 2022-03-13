8e3441ed36e0cb5d4b026a52ed594f5b7322af44
datewat <thomaszimmer524@gmail.com>
2022-03-13 21:30:10 +0100

    doing rest tmmrw

--- backend/database/main.sql
+++ backend/database/main.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE main;
 
 CREATE TABLE main.Items (
-    Id int not null auto_increment,
+    Id int not null,
     ItemName text not null,
     ItemBeschreibung text
 );
@@ -11,3 +11,10 @@ CREATE TABLE main.Users (
     UserName text not null, 
     Password text not null 
 );
+
+
+-- Der befehl muss ausgeführt werden, um diese datei auf dem server auszuführen, wenn der server läuft:
+-- mysql -u root -h localhost -P 3306 --password=password < main.sql 
+
+-- oder beim starten:
+-- docker exec mysql 'mysql -u root -h localhost -P 3306 --password=password < main.sql'
