54d9eee34c25a4a84e84f85617ba7b75591b9e80
datewat <thomaszimmer524@gmail.com>
2022-03-20 15:33:07 +0100

    changed order of setup processes

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#sudo apt install docker-compose python3  # installiert docker-compose und python3, um sicher zu gehen
+#sudo apt install docker-compose python3 python3-pip && pip install mysql-connector-python  # installiert docker-compose und python3, um sicher zu gehen
 echo ------------------------[Leere log Datei]------------------------------
 echo "" > backend/server/logging.txt 
 echo ------------------------[Baue Server]----------------------------------
 docker build -t luca -f Dockerfile .         # baut docker container mit namen luca
+echo ------------------------[Initiiere Datenbank]--------------------------
+cd backend
+docker-compose --file docker-compose.yml up -d
+cd ..
 echo ------------------------[Starte Server]--------------------------------
 docker run --rm -it -d -p 8000:8000 luca        # startet docker container
-echo ------------------------[Initiiere Datenbank]--------------------------
-docker-compose --file backend/docker-compose.yml up
 echo ========================[Alles bereit!]================================
