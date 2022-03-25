dd452b0b1bea7ea9a685c0e3c3501c6cf45d72b7
fingadumbledore <fingadumbledore@gmail.com>
2022-03-25 08:29:04 +0100

    added log_server function

--- backend/server/server.py
+++ backend/server/server.py
@@ -6,42 +6,44 @@ import logging
 from urllib import parse
 
 PORT = 8000
-datei = open('server.log','a')
 date = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))
+log = date
 
 class Serve(BaseHTTPRequestHandler):
 
-
-    def log_server():
-
-        log = date + file_to_open 
+    def log_server(log):
+        datei = open('server.log','a')
         datei.write('\n' + " " + log )
+        log = date
 
     def do_GET(self):
         if self.path == '/':
             self.path = '../../frontend/sites/index.html'
-            
+
         try:
                 file_to_open = open(self.path[1:]).read()
                 self.send_response(200)
-                log_server()
+                log = log + file_to_open
+                log_server(log)
                # datei.write('\n' + " " + date)
         except:
             file_to_open = "File not found"
             self.send_response(400)
-            log_server()
+            log = date + file_to_open
+            log_server(log)
+
             #datei.write('\n' + " " + log)
             logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
             logging.error(file_to_open)
-            
+
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
-        
+
         parsed = parse.urlparse(self.path)
-        
+
         if parsed.path == '/search':
             # convert to sql query and execute
-            print( "hier ist die query: " + parsed.query)
+            #print( "hier ist die query: " + parsed.query)
             logging.error(parsed.query)
 
     def do_POST(self):
@@ -50,13 +52,15 @@ class Serve(BaseHTTPRequestHandler):
             self.send_responses(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            
+
             self.wfile(bytes('{"time": "' + date + '"}',"utf-8"))
         except:
                 self.send_response(400)
-                print("POST error")
+                #print("POST error")
 
 httpd = HTTPServer(('0.0.0.0', PORT), Serve)
+log = log + "server is now running on" + str(PORT)
+log_server(log)
 print("server is now running on " + str(PORT))
 
 httpd.serve_forever()
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #sudo apt install docker-compose python3  # installiert docker-compose und python3, um sicher zu gehen
 echo ------------------------[Leere log Datei]------------------------------
-echo "" > backend/server/server.log 
+echo "" > backend/server/server.log
 echo ------------------------[Baue Server]----------------------------------
 docker build -t luca -f Dockerfile .         # baut docker container mit namen luca
 echo ------------------------[Starte Server]--------------------------------
 docker run --rm -it -d -p 8000:8000 luca        # startet docker container
 echo ------------------------[Initiiere Datenbank]--------------------------
-docker-compose --file backend/docker-compose.yml up
+docker-compose --file backend/docker-compose.yml up -d
 echo ========================[Alles bereit!]================================
