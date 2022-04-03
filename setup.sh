553840a8e3e39dc1c41ec014a9ecd66d4bee5326
fingadumbledore <fingadumbledore@gmail.com>
2022-04-03 07:01:35 +0200

    added reboot after instalation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#sudo apt install docker-compose python3 python3-pip && pip install mysql-connector-python  # installiert docker-compose und python3, um sicher zu gehen
+
+# Dieses Skript ist dafür zuständig, die benötigte Umgebung für die Datenbank und den Server zu erstellen.
+# Es werden folgende Packete geladen:
+# Python3,
+# docker-compose,
+# python3 pip,
+# mysql-connector-python.
+# Folgende Ordner werden erstellt:
+# /etc/systemd/system/wserver.service
+
 bash clean.sh
 echo ------------------------[Überprüfung der Berechtigung]-----------------
 if [ `/usr/bin/id -u` != "0" ]; then
@@ -17,6 +26,12 @@ else
     exit 0
     
 fi
+echo ------------------------[Prüfe ob System aktuell ist]------------------
+sudo apt update && sudo apt upgrade && sudo apt full-upgrade
+
+echo ------------------------[Installiere zusätzliche Packete]--------------
+sudo apt install docker-compose python3 python3-pip && pip install mysql-connector-python
+
 echo ------------------------[Erstelle Service]-----------------------------
 sudo touch /etc/systemd/system/wserver.service
 
@@ -31,7 +46,6 @@ echo " [Install]"  >> /etc/systemd/system/wserver.service
 echo " WantedBy = multi-user.target" >> /etc/systemd/system/wserver.service
 echo ------------------------[Service wurde erstellt]-----------------------
 
-
 echo ------------------------[Leere log Datei]------------------------------
 echo "" > backend/server/server.log
 
@@ -39,6 +53,12 @@ cd /etc/systemd/system
 systemctl enable wserver.service
 
 echo ------------------------[Service wurde gestartet]---------------------
+
+echo -----------------------[Fertigstellung]------------------------------- # der Neustart ist dafür dar, dass der Service 1A funktioniert
+echo Vielen Dank das sie sich entschieden haben, mit Bash zu fliegen. Wir wünschen ihnen noch einen schönen Aufenthalt Adios 
+echo $HOSTNAME wird automatisch neugestartet
+sleep 10
+# sudo reboot now
 
 #echo ------------------------[Baue Server]----------------------------------
 #docker build -t luca -f Dockerfile .         # baut docker container mit namen luca
