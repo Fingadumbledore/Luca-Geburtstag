fbf4f32242447f55dee928a9b6925b5178fafe57
fingadumbledore <fingadumbledore@gmail.com>
2022-04-03 10:14:09 +0200

    removed docker in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 sudo systemctl disable wserver.service
 rm /etc/systemd/system/wserver.service
-docker stop --force /backend-db-1 
-docker stop --force luca
-docker rmi luca --force
-docker rmi mysql --force
