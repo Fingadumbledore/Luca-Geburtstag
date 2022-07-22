07a29524df95ab91e5bad6a44fe9eea0bace2bb9
fingadumbledore <fingadumbledore@gmail.com>
2022-07-22 17:49:10 +0200

    update setup.py

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Dieses Skript ist dafür zuständig, die benötigte Umgebung für die Datenbank und den Server zu erstellen.
-# Es werden folgende Packete geladen:
-# Python3,
-# python3 pip,
-# mysql-connector-python.
-# Folgende Ordner werden erstellt:
-# /etc/systemd/system/wserver.service
-
-bash clean.sh
 echo ------------------------[Überprüfung der Berechtigung]-----------------
 if [ `/usr/bin/id -u` != "0" ]; then
       echo  fail "Du brauchst Rechte für dieses Skript\n"
@@ -17,40 +8,19 @@ else
     echo Deine Rechte passen
 fi
 
-echo ------------------------[schaue ob Dateien bereits existieren]---------
-if [ ! -f /etc/systemd/system/wserver.service ]; then
-    echo "Datei wurde nicht gefunden"
-else 
-    echo "Datei existiert bereits"
-    exit 0
-    
-fi
 echo ------------------------[Prüfe ob System aktuell ist]------------------
 sudo apt update && sudo apt upgrade && sudo apt full-upgrade
 
 echo ------------------------[Installiere zusätzliche Packete]--------------
-sudo apt install docker-compose python3 python3-pip && sudo apt install sqlite3
-
-echo ------------------------[Erstelle Service]-----------------------------
-cd /etc/systemd/system
-touch wserver.service
-sudo mv wserver.service /etc/systemd/system
-
-
-echo ------------------------[Service wurde erstellt]-----------------------
+sudo apt install python3 python3-pip && sudo apt install sqlite3
 
 echo ------------------------[Leere log Datei]------------------------------
 echo "" > backend/server/server.log
 
-cd /etc/systemd/system
-
-#systemctl start wserver.service
-sudo systemctl enable wserver.service
-
-echo ------------------------[Service wurde gestartet]---------------------
-
-echo -----------------------[Fertigstellung]------------------------------- # der Neustart ist dafür dar, dass der Service 1A funktioniert
+echo -----------------------[Fertigstellung]------------------------------- 
 echo Vielen Dank das sie sich entschieden haben, mit Bash zu fliegen. Wir wünschen ihnen noch einen schönen Aufenthalt Adios 
-echo $HOSTNAME wird automatisch neugestartet 
-sleep 10
-# sudo reboot now
+sleep 5
+echo -----------------------[Starte Server]---------------------------------
+cd backend/server
+python3 database.py
+python3 server.py
