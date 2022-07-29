e69dcef696a678795ca730e872c3b49530a21a7d
hyperbel <thomaszimmer524@gmail.com>
2022-07-29 21:00:08 +0200

    removed warnings

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,8 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import http.server
-import socketserver
 import time
-import logging
 import sqlite3
 from urllib import parse
 import json
@@ -10,35 +7,36 @@ import json
 PORT = 8000
 date = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))
 log = date
- 
+
 connection = sqlite3.connect("login.db")
 
 # Datensatz-Cursor erzeugen
 cursor = connection.cursor()
 
+
 class Serve(BaseHTTPRequestHandler):
     def log_server(self, log):
-        datei = open('server.log','a')
-        datei.write('\n' + " " + log )
+        datei = open('server.log', 'a')
+        datei.write('\n' + " " + log)
         log = date
         datei.close()
 
     def do_GET(self):
-        self.sites = '../../../frotend/sites'
         parsed = parse.urlparse(self.path)
-        match self.path: 
-            case '/': 
-                self.path = f'{self.sites}/index.html'
-            case '/matrix':
-                self.path  = '../../../frontend/sites/matrix.html'
-            case '/search':
-                self.search(parsed.query)
-                self.path = '../../.../frontend/'
-            case '/login':
-               	self.login()
+        print(self.path)
+        if self.path == '/':
+            self.path = '../../../frontend/sites/index.html'
+        if self.path == '/matrix':
+            self.path = '../../../frontend/sites/matrix.html'
+        if self.path == '/search':
+            print('asdf')
+            print(parsed.query)
+            self.search(parsed.query)
+            self.path = '../../.../frontend/results.html'
+        if self.path == '/login':
+            self.login()
 
         try:
-
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
             log = date + " 200"
@@ -46,25 +44,21 @@ class Serve(BaseHTTPRequestHandler):
         except:
             file_to_open = "File not found"
             self.send_response(400)
-            log = date + " " +file_to_open
+            log = date + " " + file_to_open
         finally:
-            
             self.log_server(log)
-            #datei.write('\n' + " " + log)
-            #logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-            #logging.error(file_to_open)
 
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
-      
+
     def search(self, q):
         # convert to sql query and execute
-        print( "hier ist die query: " + q)
+        print("hier ist die query: " + q)
         # connect to db
         verbindung = sqlite3.connect("login.db")
+        print("cursort")
         zeiger = verbindung.cursor()
-        print(json.dumps(urlparse.parse_qs(q)))
-        qjs = json.dumps(urlparse.parse_qs(q))
+        qjs = json.dumps(parse.urlparse.parse_qs(q))
         print(qjs)
 
 #        by_what = match qjs["search-type"]:
@@ -74,10 +68,10 @@ class Serve(BaseHTTPRequestHandler):
 #            break
 #       case "byDescription": "ItemBeschreibung"
 #          break
-        
-        #match qjs["search-type"]
-        
-        query = f"select * from user where {by_what} is {qjs['query']}"
+
+        # match qjs["search-type"]
+
+        query = f"select * from user where !!!' is {qjs['query']}"
         zeiger.execute(query)
         inhalt = zeiger.fetchall()
         print(inhalt)
@@ -86,16 +80,17 @@ class Serve(BaseHTTPRequestHandler):
     def login():
         print("moin")
 
+
 try:
     httpd = HTTPServer(('0.0.0.0', PORT), Serve)
     log = log + "server is now running on 127.0.0.1:" + str(PORT)
-    #log_server(log)
+    # log_server(log)
     print("server is now running on http://127.0.0.1:" + str(PORT))
 
     httpd.serve_forever()
 except KeyboardInterrupt:
-        pass
-    #Server beenden, und Datein speichern/in Sammlung schieben
-        httpd.server_close()
-        print("\nServer stopped.")
-        print("saving files")
+    pass
+    # Server beenden, und Datein speichern/in Sammlung schieben
+    httpd.server_close()
+    print("\nServer stopped.")
+    print("saving files")
