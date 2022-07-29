4add4ddd1e0a1acf1fe305e26b2fe54c926d87e5
hyperbel <thomaszimmer524@gmail.com>
2022-07-29 16:52:06 +0200

    added arch linux setup file

---
+++ setup-arch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ------------------------[Überprüfung der Berechtigung]-----------------
+if [ `/usr/bin/id -u` != "0" ]; then
+      echo  fail "Du brauchst Rechte für dieses Skript"
+      exit 0
+else 
+    echo Deine Rechte passen
+fi
+
+echo ------------------------[Prüfe ob System aktuell ist]------------------
+sudo pacman -Syu python python3-pip sqlite3 docker docker-compose
+
+echo ------------------------[Leere log Datei]------------------------------
+echo "" > backend/server/server.log
+
+echo -----------------------[Fertigstellung]------------------------------- 
+echo Vielen Dank das sie sich entschieden haben, mit Bash zu fliegen. Wir wünschen ihnen noch einen schönen Aufenthalt Adios 
+sleep 5
+echo -----------------------[Starte Server]---------------------------------
+cd backend/server
+python3 database.py
+python3 server.py
