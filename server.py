d833fd70a693303d7fda3ef576adcf9741f0a626
hyperbel <thomaszimmer524@gmail.com>
2022-08-01 18:43:13 +0200

    serve html files

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+import json
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "<h1>index called</h1>\n<h2>hier muss ne datei hin, kein plaintext</h2>"
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
