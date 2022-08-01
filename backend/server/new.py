5c8566ff1e34c06dd9fb20f2df3304aa90302730
hyperbel <thomaszimmer524@gmail.com>
2022-08-01 10:33:33 +0200

    test

--- backend/server/new.py
+++ backend/server/new.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend/sites/')
+
 
 @app.route("/")
 def index():
-    return "<h1>index called</h1>\n<h2>hier muss ne datei hin, kein plaintext</h2>"
+    return app.send_static_file('../../../frontend/static/index.html')
+
+
+@app.route("/matrix")
+def matrix():
+    return render_template("<h1>matrix</h1>")
+
+
+@app.route("/search")
+def search():
+    return "<h1>test</h1>"
+
+
+@app.route("/login")
+def login_r():
+    return login()
+
+
+def login():
+    return render_template("<h1>asd</h1>")
