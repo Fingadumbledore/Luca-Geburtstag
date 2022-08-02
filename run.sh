79a38b775ecd2121086df14bc336bccdf3e04411
hyperbel <thomaszimmer524@gmail.com>
2022-08-02 13:51:09 +0200

    pass browser to run script

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 export FLASK_APP=server.py
-flask run& sleep 3& firefox 127.0.0.1:5000
+if [ -z $@ ]; then
+  echo "please pass browser as argument."
+  exit 0
+fi
+
+flask run& sleep 3& $@ 127.0.0.1:5000
